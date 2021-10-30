3126147838163a4e3a90197bccb5ba1aa726e01b
Gerson Azabache Martinez <gerson_dunk@hotmail.com>
2021-10-30 10:21:39 -0500

    connection string

---
+++ NetCoreAjax/Migrations/20211030070935_initial.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace NetCoreAjax.Migrations
+{
+    public partial class initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Movimientos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    NumeroCuenta = table.Column<string>(type: "varchar(20)", nullable: true),
+                    Referencia = table.Column<string>(type: "varchar(50)", nullable: true),
+                    Cantidad = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Movimientos", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Movimientos");
+        }
+    }
+}
