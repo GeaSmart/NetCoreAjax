eb4c8432f446550e4cdc8f8d52f9689efae21e14
Gerson Azabache Martinez <gerson_dunk@hotmail.com>
2021-10-30 02:02:49 -0500

    initial

---
+++ NetCoreAjax/Entities/Movimiento.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NetCoreAjax.Entities
+{
+    public class Movimiento
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Column(TypeName = "varchar(20)")]
+        public string NumeroCuenta { get; set; }
+
+        [Column(TypeName = "varchar(50)")]
+        public string Referencia { get; set; }
+                
+        public int Cantidad { get; set; }
+    }
+}
---
+++ NetCoreAjax/Models/ApplicationDBContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using NetCoreAjax.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NetCoreAjax.Models
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        public DbSet<Movimiento> Movimientos { get; set; }
+    }
+}
